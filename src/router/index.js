ab8d50d37bf51e06095c9116e987767249905ad9
chenkuanchen <109160858+chenkuanchen@users.noreply.github.com>
2026-09-15 16:39:36 +0800

    Lazy-load route components
    Replace static view imports in src/router/index.js with dynamic import() functions for each route. This enables code-splitting and reduces initial bundle size while preserving existing routing behavior.

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,28 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import InfoView from '@/views/InfoView.vue'
-import ProjectView from '@/views/ProjectView.vue'
-import WorksView from '@/views/WorksView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/works', name: 'works', component: WorksView },
-    { path: '/project/:id', name: 'project', component: ProjectView },
-    { path: '/info', name: 'info', component: InfoView },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/works',
+      name: 'works',
+      component: () => import('@/views/WorksView.vue'),
+    },
+    {
+      path: '/project/:id',
+      name: 'project',
+      component: () => import('@/views/ProjectView.vue'),
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: () => import('@/views/InfoView.vue'),
+    },
   ],
   scrollBehavior(to) {
     if (to.hash) {
